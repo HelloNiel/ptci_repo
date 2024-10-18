1a58ec79936312b1b99789a15eac44c3456ec0f4
devstygian <hackstygian@gmail.com>
2024-10-18 10:31:09 +0800

    Renamed the Copyright Section

--- judge/includes/footer.php
+++ judge/includes/footer.php
@@ -1,5 +1,5 @@
 <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                            <div class="text-muted">Copyright &copy; PTCI-IC2 2024</div>
                         </div>
                     </div>
